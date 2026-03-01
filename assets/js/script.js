b3adf79f04d573a3e1c118a5b0dea5d765417d39
NisRin <sahoulinisrine@gmail.com>
2026-03-01 20:26:40 +0100

    header avec nav dropdown sans changement nav

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+// CAROUSEL
+
+const slides = document.querySelectorAll(".slide");
+const uptitle = document.getElementById("uptitle");
+const title = document.getElementById("title");
+const text = document.getElementById("text");
+const btn1 = document.getElementById("btn1");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+let index = 0;
+
+function updateSlide(i) {
+
+  // enlever active partout
+  for (let slide of slides) {
+    slide.classList.remove("active");
+  }
+
+  // activer slide
+  slides[i].classList.add("active");
+
+  // récupérer data
+  const bg = slides[i].dataset.bg;
+  const u = slides[i].dataset.uptitle;
+  const t = slides[i].dataset.title;
+  const p = slides[i].dataset.text;
+  const b = slides[i].dataset.btn1;
+
+  // appliquer background
+  slides[i].style.backgroundImage = "url(" + bg + ")";
+
+  // changer contenu
+  uptitle.textContent = u;
+  title.textContent = t;
+  text.textContent = p;
+  btn1.textContent = b;
+}
+
+next.addEventListener("click", function() {
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
+  updateSlide(index);
+});
+
+prev.addEventListener("click", function() {
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  updateSlide(index);
+});
+
+updateSlide(index);
+
+
+// Lien en orange
+
+const liens = document.querySelectorAll('.pagesNav a');
+
+liens.forEach(lien => {
+    lien.addEventListener('mouseover', function() {
+        this.style.color = 'orange';
+    });
+    lien.addEventListener('mouseout', function() {
+        this.style.color = 'white';
+    });
+});
+
+// dropdown dans la nav
+
+const dropdown = document.querySelector(".dropdown");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+dropdown.addEventListener("mouseover", function() {
+    dropdownMenu.classList.remove("hidden");
+    dropdownMenu.classList.add("visible");
+});
+
+dropdown.addEventListener("mouseout", function() {
+    dropdownMenu.classList.add("hidden");
+    dropdownMenu.classList.remove("visible");
+});
